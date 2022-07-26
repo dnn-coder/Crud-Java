22e568ce73d5c0567327d6c25b89bc0fb95cb183
dnn-coder <dvivasmera1.2@gmail.com>
2022-07-26 08:39:36 -0500

    first commit

---
+++ src/crudjava/CrudJava.java
@@ -0,0 +1,9 @@
+package crudjava;
+
+public class CrudJava {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/crudjava/empleadosBL/empleadosBL.java
@@ -0,0 +1,30 @@
+package crudjava.empleadosBL;
+
+public class empleadosBL {
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    int ID ;
+    String nombre, correo;
+}
